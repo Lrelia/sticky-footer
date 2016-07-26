00e070cc9d399b7633f237c3414054d2dc6d093e
吴蔼仪 <susan.wu@vipshop.com>
2016-07-26 16:45:41 +0800

    粘连 Footer 的 5 种方法

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+app.get('/sample-0', function (req, res) {
+    res.sendfile('./sample-0.html');
+});
+
+app.get('/sample-1', function (req, res) {
+    res.sendfile('./sample-1.html');
+});
+
+app.get('/sample-2', function (req, res) {
+    res.sendfile('./sample-2.html');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+var server = app.listen(80, function () {
+  var host = "localhost";
+  var port = 80;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
